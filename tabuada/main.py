4c74c3dbf82341b57b732d4ad8b025b5602be129
jonbrand-nautilus <jonbranaraujo18@gmail.com>
2025-09-01 14:46:20 -0300

    feat: adding formatting with f strings

--- tabuada/main.py
+++ tabuada/main.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
 """Imprime a tabuada do 1 ao 10."""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Jonatas"
 
 factors = list(range(1,11))
+separator = (30*"#")
+template = """
+------Tabuada do número: {factor}------
 
-for factor in factors:
-    print("Tabuada do número:", factor)
-    for newFactor in factors:
-        print(factor, " x ", newFactor, " = ", factor * newFactor)
-print("----------------------------")
+{operations}
+
+{separator}
+"""
+
+for factor1 in factors:
+	operations = ""
+	for newFactor in factors:
+			line = ""
+			result = factor1 * newFactor
+			line += f"{factor1} x {newFactor} = {result}"
+			operations += f"{line:^30}\n"
+	print(
+		template.format(
+			factor=factor1,
+			operations=operations,
+			separator=separator
+		)
+	)
