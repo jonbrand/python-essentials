f7a337810af4af168727dce76f4ff48417874558
jonbrand-nautilus <jonbranaraujo18@gmail.com>
2025-12-01 12:13:25 -0300

    exercicios: criação do alerta de temperatura

---
+++ temperature_alert/temperature_alert.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Alarme de temperatura
+
+Faça um script que pergunta ao usuário qual a temperatura atual e o indice de umidade do ar para que seja exibida uma mensagem de alerta dependendo 
+das condições:
+
+- temp maior 45: "ALERTA!!! 🥵 Perigo calor extremo"
+
+- temp maior que 30 e temp vezes 3 for maior ou igual a umidade: "ALERTA!!! 🥵♒ Perigo de calor úmido"
+
+- temp entre 10 e 30: "😀 Normal"
+
+- temp entre 0 e 10: "🥶 Frio"
+
+- temp <0: "ALERTA!!! ⛄ Frio Extremo."
+
+ex:
+
+python3 temp.py 
+temperatura: 30
+umidade: 90
+... 
+"ALERTA!!! 🥵 Perigo calor extremo"
+"""
+import sys
+import logging
+
+log = logging.Logger("Alerta")
+
+info = {
+    "temperatura": None,
+    "umidade": None
+}
+
+keys = info.keys()
+
+for key in keys:
+    try:
+        info[key] = float(input(f"Qual é a {key.title()}: ").strip())
+    except ValueError:
+        log.error(f"{key.title()} invalido!")
+        sys.exit(1)
+
+temp = info["temperatura"]
+moise = info["umidade"]
+temp_mois = temp * 0.3
+
+if temp >= 45:
+    print("ALERTA!!! 🥵 Perigo calor extremo")
+elif temp > 30 and temp_mois >= moise:
+    print("ALERTA!!! 🥵♒ Perigo de calor úmido")
+elif temp >= 30 and temp_mois < moise:
+    print("ALERTA!!! 🥵♒ Perigo de calor")
+elif temp >= 10 and temp <= 30:
+    print("😀 Normal")
+elif temp >= 0 and temp <= 10:
+    print("🥶 Frio")
+elif temp < 0:
+    print("ALERTA!!! ⛄ Frio Extremo.")
