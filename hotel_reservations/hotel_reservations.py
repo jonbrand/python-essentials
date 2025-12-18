eb3964a17430e1439d662637508d32155845229a
jonbrand-nautilus <jonbranaraujo18@gmail.com>
2025-12-18 19:49:48 -0300

    exercícios: corrigindo estrutura de dados e validações no hotel reservations

--- hotel_reservations/hotel_reservations.py
+++ hotel_reservations/hotel_reservations.py
@@ -26,51 +26,59 @@ __license__ = "Unlicense"
 
 import os
 
+# Path dos arquivos e diretório atual
 path = os.curdir
 room_filepath = os.path.join(path, "rooms.txt")
 reservation_filepath = os.path.join(path, "reservation.txt")
 
+# Estrutura de dados (Dicionários) que lidam com os quartos e as reservas
 rooms = {}
 reservation = {}
 
+# Loop que inicia o script
+
 while True:
     user_name = input("Seja bem-vindo ao Hotel_Terminal!\n(Caso queira sair pressione enter para sair)\n\nQual é o seu nome?\n").strip()
+    
     if user_name == "":
         break
 
+    # Tenta ler o arquivo com as reservas existentes
     try:
         with open(reservation_filepath) as file_:
             for line in file_:
                 client, code, days_reserved, booking_total = line.strip().split(',')
-                reservation[code] = {"Nome do Cliente", client, "Dias de Reserva", days_reserved, "Total da Reserva", booking_total}
+                reservation[code] = {"Nome do Cliente": client, "Dias de Reserva": days_reserved, "Total da Reserva": booking_total}
     except:
         print("Arquivo vázio.")
 
+    # Tenta ler o arquivo contendo todos os quartos do hotel e outras informações 
     try:
         with open(room_filepath) as file_:
             for line in file_:
                 code, room_name, price = line.strip().split(',')
-                rooms[code] = {"Nome da Suíte", room_name, "Preço", int(price)}
+                rooms[code] = {"Nome da Suíte": room_name, "Preço": int(price)}
     except:
         print("Arquivo vázio.")
 
-    # TODO: Preciso extrair o preço do quarto em questão e passar  valor corretor para uma variável que vai ser utilizada no calculo do valor total da reserva.
+    # Como posso acessar o valor do preço do quarto selecionado pelo usuário?
     
+    # Cópia da estrutura de dados que lida com os quartos
     rooms_available = rooms.copy()
+    
+    # Percorre a estrutura de dados que lida com os quartos para remover dali quando um quarto for reservado
     for line in rooms:
         if line in reservation:
             rooms_available.pop(line)
 
-
+    # Inputs do usuário
     # TODO: Mudar formato de apresentação dos quartos disponíveis
     print(f"Quartos disponíveis: {rooms_available.keys()}")
     room_number = input("Selecione um número de quarto: ")
     days_reserved = input("Quantos dias você gostaria de reservar o quarto?: ").strip()
-    booking_total = int(price) * int(days_reserved)
-    reservation = (user_name, room_number, days_reserved, booking_total)
-    print(reservation)
-    
-
+    booking_total = int(rooms[room_number]["Preço"]) * int(days_reserved)
+    # reservation = (user_name, room_number, days_reserved, booking_total)
+    # Tenta adicionar uma reserva ao arquivo de reservas
     try:
         with open(reservation_filepath, "a") as file_:
             file_.write(f"{user_name},{room_number},{days_reserved},{booking_total}\n")
@@ -86,11 +94,10 @@ while True:
     if finish_booking == "n":
         break
     else:
-        continue
 
-    next_step = input("Gostaria de fazer mais alguma reserva? [Y,n]").strip()
-    
-    if next_step == "n":
-        break
-    else:
-        continue
+        next_step = input("Gostaria de fazer mais alguma reserva? [Y,n]").strip()
+        
+        if next_step == "n":
+            break
+        else:
+            continue
